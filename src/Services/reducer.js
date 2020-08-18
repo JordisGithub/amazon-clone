cf6de7653a0012262e7a67aafb139098f279d070
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-08-17 21:50:41 -0400

    update remove from basket button function

--- src/Services/reducer.js
+++ src/Services/reducer.js
@@ -1,22 +1,22 @@
 export const initialState = {
   basket: [
-    {
-      id: "12321341",
-      title: "Honeybee Gardens 'Skinny Dip' Refillable Eye Shadow Palette : Natural Ingredients : Gluten Free, Vegan, Paraben Free",
-      price: 27.99,
-      rating: 4,
-      image: "https://images-na.ssl-images-amazon.com/images/I/41xecyG-lbL.jpg",
-      link: "https://www.amazon.com/Honeybee-Gardens-Skinny-Refillable-Palette/dp/B078P1QF2C/ref=sr_1_89?dchild=1&keywords=vegan+makeup&qid=1597628877&sr=8-89"
-    },
-    {
+    // {
+    //   id: "12321341",
+    //   title: "Honeybee Gardens 'Skinny Dip' Refillable Eye Shadow Palette : Natural Ingredients : Gluten Free, Vegan, Paraben Free",
+    //   price: 27.99,
+    //   rating: 4,
+    //   image: "https://images-na.ssl-images-amazon.com/images/I/41xecyG-lbL.jpg",
+    //   link: "https://www.amazon.com/Honeybee-Gardens-Skinny-Refillable-Palette/dp/B078P1QF2C/ref=sr_1_89?dchild=1&keywords=vegan+makeup&qid=1597628877&sr=8-89"
+    // },
+    // {
 
-      id: "12321341",
-      title: "Honeybee Gardens 'Skinny Dip' Refillable Eye Shadow Palette : Natural Ingredients : Gluten Free, Vegan, Paraben Free",
-      price: 27.99,
-      rating: 4,
-      image: "https://images-na.ssl-images-amazon.com/images/I/41xecyG-lbL.jpg",
-      link: "https://www.amazon.com/Honeybee-Gardens-Skinny-Refillable-Palette/dp/B078P1QF2C/ref=sr_1_89?dchild=1&keywords=vegan+makeup&qid=1597628877&sr=8-89"
-    },
+    //   id: "12321341",
+    //   title: "Honeybee Gardens 'Skinny Dip' Refillable Eye Shadow Palette : Natural Ingredients : Gluten Free, Vegan, Paraben Free",
+    //   price: 27.99,
+    //   rating: 4,
+    //   image: "https://images-na.ssl-images-amazon.com/images/I/41xecyG-lbL.jpg",
+    //   link: "https://www.amazon.com/Honeybee-Gardens-Skinny-Refillable-Palette/dp/B078P1QF2C/ref=sr_1_89?dchild=1&keywords=vegan+makeup&qid=1597628877&sr=8-89"
+    // },
   ],
   user: null,
 };
